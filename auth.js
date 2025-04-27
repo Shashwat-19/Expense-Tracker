d60a6d7f555a37664a6b5ba07fa026b026fb385a
Shashwat-19 <shashwat1956@gmail.com>
2025-04-27 17:37:53 +0530

    Added path

--- auth.js
+++ auth.js
@@ -77,7 +77,7 @@ if (registerForm) {
             console.log("Document successfully written!");
             showMessage("User created successfully! Redirecting...");
             setTimeout(() => {
-                window.location.href = "./tracker/index.html";
+                window.location.href = "./tracker/exptr.html";
             }, 1500);
           })
           .catch((error) => {
